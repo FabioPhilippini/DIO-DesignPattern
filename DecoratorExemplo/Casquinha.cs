8e3532c8d7ca951442ec2a73089824bd0ef76a01
Fabio Philippini <fabioandre381@gmail.com>
2021-01-12 17:04:13 -0300

    Classe e interface criadas

---
+++ DecoratorExemplo/Casquinha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorExemplo
+{
+    public class Casquinha
+    {
+        public string BuscarCasquinha()
+        {
+            var casquinha = "Esta é uma casquinha";
+            return casquinha;
+        }
+    }
+}
---
+++ DecoratorExemplo/Interfaces/ICasquinha.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorExemplo.Interfaces
+{
+    public interface ICasquinha
+    {
+        string BuscarCasquinha();
+    }
+}
